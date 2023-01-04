b78c2f03591cae5e81e2febdf5c929dc1248bc9d
sperimental3 <mosconi.matteo@virgilio.it>
2023-01-04 14:55:46 +0100

    listening part improved

--- listening.py
+++ listening.py
@@ -1,5 +1,3 @@
-# TODO try to improve this part in general
-
 import speech_recognition as sr
 from ctypes import *
 from contextlib import contextmanager
@@ -28,24 +26,50 @@ def noalsaerr():
 
 
 class Listener:
+    """Even if not used as a class but only via static method call,
+    I prefer to maintain the class for code coherence."""
     def __int__(self):
-        #self.recognizer = sr.Recognizer()
+        # self.recognizer = sr.Recognizer()
 
-        #self.mic = sr.Microphone()
-        #print(self.mic)
+        # self.mic = sr.Microphone()
+        # print(self.mic)
         pass
 
     @staticmethod
     def listen():
         recognizer = sr.Recognizer()
+        recognizer.energy_threshold = 50
 
         with noalsaerr(), sr.Microphone() as source:
             print("Tell me what you want!")
             recognizer.adjust_for_ambient_noise(source, duration=0.5)
             audio = recognizer.listen(source)
 
-        phrase = recognizer.recognize_google(audio)
-        print(phrase)
+        phrase_list = recognizer.recognize_google(audio, show_all=True)
+
+        # Here a brief way to rank the possible phrases listened by the recognizer,
+        # trying to rank first the one with more words that are relevant to my task.
+        chosen_phrase = ""
+        max_points = 0
+
+        for phrase in phrase_list["alternative"]:
+            keywords = [word for word in phrase["transcript"].split() if word in ["gin", "vermut", "lemon", "campari", "Baxter", "Campari", "Negroni", "negroni", "martini", "Martini"]]
+            points = len(keywords)
+            if points > max_points:
+                max_points = points
+                chosen_phrase = phrase["transcript"]
+
+        # print(phrase_list)
         # print(type(phrase))
 
-        return phrase
+        # print(chosen_phrase)
+
+        return chosen_phrase
+
+
+"""
+if __name__ == "__main__":
+    Ears = Listener()
+    phrase = Ears.listen()
+    print(phrase)
+"""
